52d51e2909718b9b994250f639e02b565a8e37c4
Zachd101 <zachary.dawood@ucc.on.ca>
2017-12-02 20:00:57 -0500

    Server stuff

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+(let ((file-name-handler-alist nil))
 (setq-default gc-cons-threshold 100000000)
 
 ;;(package-initialize)
@@ -23,3 +24,22 @@
  )
 (put 'narrow-to-region 'disabled nil)
 (setq-default gc-cons-threshold 800000)
+)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" "cdfc5c44f19211cfff5994221078d7d5549eeb9feda4f595a2fd8ca40467776c" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" default)))
+ '(org-agenda-files (quote ("~/Google Drive/_.org")))
+ '(package-selected-packages
+   (quote
+    (emms ac-org zenburn-theme yasnippet which-key web-mode use-package speed-type spacegray-theme ox-twbs magit iedit hungry-delete ace-window ac-helm))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
