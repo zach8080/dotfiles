a12aff9f81bba24a6923f7e6e0a61ce6d1b2ed5b
Zachd101 <zachary.dawood@ucc.on.ca>
2018-04-02 13:14:18 -0400

    idk
    Revert "Multi-term fix"
    
    This reverts commit 406cde5b8a04c54d2c71d4afb944efc9aa5b619a.

--- init.el
+++ init.el
@@ -10,19 +10,36 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" "cdfc5c44f19211cfff5994221078d7d5549eeb9feda4f595a2fd8ca40467776c" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" default)))
+    ("cdfc5c44f19211cfff5994221078d7d5549eeb9feda4f595a2fd8ca40467776c" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" default)))
  '(org-agenda-files (quote ("~/Google Drive/_.org")))
  '(package-selected-packages
    (quote
-    (yasnippet-snippets emms ac-org zenburn-theme yasnippet which-key web-mode use-package speed-type spacegray-theme ox-twbs magit iedit hungry-delete ace-window ac-helm)))
- '(pdf-tools-handle-upgrades nil))
+    (emms ac-org zenburn-theme yasnippet which-key web-mode use-package speed-type spacegray-theme ox-twbs magit iedit hungry-delete ace-window ac-helm))))
+ '(org-agenda-files (quote ("~/Google Drive/_.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
 (put 'narrow-to-region 'disabled nil)
 (setq-default gc-cons-threshold 800000)
 )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" "cdfc5c44f19211cfff5994221078d7d5549eeb9feda4f595a2fd8ca40467776c" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" default)))
+ '(org-agenda-files (quote ("~/Google Drive/_.org")))
+ '(package-selected-packages
+   (quote
+    (emms ac-org zenburn-theme yasnippet which-key web-mode use-package speed-type spacegray-theme ox-twbs magit iedit hungry-delete ace-window ac-helm))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
